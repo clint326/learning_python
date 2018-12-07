5e99f3521533b61d03b4d41b0d1fe40e5aaf348d
Clint Laskowski <clint.laskowski@gmail.com>
2018-12-07 02:40:29 -0600

    Create prime_numbers.py

---
+++ prime_numbers.py
@@ -0,0 +1,20 @@
+def is_prime(x):
+    if type(x) is int and x >= 2:
+        for i in range(2, x):
+            if x % i == 0:
+                return False
+        else:
+            return True
+    else:
+        return False
+    
+def prime100():
+    c = 0
+    i = 0
+    while c < 100:
+        if is_prime(i):
+            print(i)
+            c = c + 1
+        i = i + 1
+
+prime100()
