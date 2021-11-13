82e6a7e3965adc7625359da0a7f84b61923465c6
Clint Laskowski <clint.laskowski@gmail.com>
2021-11-13 16:26:14 -0600

    Update api_main.py

--- api_main.py
+++ api_main.py
@@ -1,6 +1,13 @@
+# first import the library that will allow us to do API requests
 import requests
 
+# next make a simple call to the Random Fox api
 response = requests.get("http://randomfox.ca/floof")
+
+# next print the response as text and as json
 print(response.text)
+print (response.json())
+
+# next assign variable 'fox' to the json output and print the image URL
 fox = response.json()
-print(fox['link'])
+print(fox['image'])
